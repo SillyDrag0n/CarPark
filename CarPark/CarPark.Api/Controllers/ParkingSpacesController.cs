9b6163a1139e808da6f7656ed47b80b46fba8ae2
SilvanMüller <silvanmueller2004@gmx.ch>
2023-01-03 13:56:23 +0100

    add CarPark.Api

---
+++ CarPark/CarPark.Api/Controllers/ParkingSpacesController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarPark.Api.Controllers
+{
+    public class ParkingSpacesController : Controller
+    {
+        //Add Route according to the API specification
+    }
+}
