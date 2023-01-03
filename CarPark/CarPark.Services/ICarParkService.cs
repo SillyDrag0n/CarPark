8610f4dca92b7567411ccf164cf80b9b6bc7008e
SilvanMüller <silvanmueller2004@gmx.ch>
2023-01-03 16:19:07 +0100

    add ICarParkService and other stuff

---
+++ CarPark/CarPark.Services/ICarParkService.cs
@@ -0,0 +1,9 @@
+﻿namespace CarPark.Services
+{
+    public interface ICarParkService
+    {
+        public int createParkingSpace((int, int) parkingSpace);
+        public int updateParkingSpace((int, int) parkingSpace);
+        public int deleteParkingSpace((int, int) parkingSpace);
+    }
+}
